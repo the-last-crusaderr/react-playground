0c679e5ab35adbed645a811b659e829073769b31
the-last-crusaderr <guptasumit922@gmail.com>
2025-01-16 22:49:28 +0530

    first version of voting-app is done

---
+++ voting_app/public/js/app.js
@@ -0,0 +1,22 @@
+
+
+
+class ProductList extends React.Component{
+ render(){
+   return (	
+    <div className="ui unstackable item">
+      <p>Hello friend !! I am a React component which cleanly maps to UI component</p>
+    </div>
+    );
+	}
+
+}
+
+
+
+ReactDOM.render(<ProductList/>,
+        document.getElementById("content"));
+
+
+
+
