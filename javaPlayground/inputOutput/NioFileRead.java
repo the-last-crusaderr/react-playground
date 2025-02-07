7ce051ea9a73d65c496d5f04ac444e66a6ee1fe4
the-last-crusaderr <guptasumit922@gmail.com>
2025-02-07 10:55:39 +0530

    end of IO module

--- javaPlayground/inputOutput/NioFileRead.java
+++ javaPlayground/inputOutput/NioFileRead.java
@@ -4,10 +4,9 @@ import java.nio.*:
 class NioFileWrite{
 
 
-
 public static void main(String[] args){
  
- Path path = Path.get("nOut");
+ Path path = Path.get("output.txt");
  List<String> String = List.of("New way"."To","Write");
 
  Files.Write(path,content):
