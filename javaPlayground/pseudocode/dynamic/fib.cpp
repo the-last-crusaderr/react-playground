22a4a36337afb0a032180bb2672a287711638cfb
the-last-crusaderr <guptasumit922@gmail.com>
2025-02-25 16:38:07 +0530

    fib

--- javaPlayground/pseudocode/dynamic/fib.cpp
+++ javaPlayground/pseudocode/dynamic/fib.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 
+
 int arr[100];
+int countFun;
+int countRaw;
 
 int fib(int n){
    
+   countFun ++;
    if(arr[n] != -1)
      return arr[n];	   
 
@@ -17,9 +21,22 @@ int fib(int n){
 
 	}
 
+int fibRaw(int n){
+  countRaw++;
+  if( n==1 || n==2)
+    return 1;
+
+  return fibRaw(n-1) + fibRaw(n-2);  
+	}
+
+
+
 
 int main(){
    memset(arr, -1, sizeof(arr));
-   cout << fib(20) << endl;
+    cout << fib(20) << endl;
+   cout << fibRaw(20) << endl;
+   cout << "fib is called " << countFun << " times with memoisation." << endl;
+   cout << "fib is called " << countRaw << " times without memoisation." << endl;
 
-	}
+}
