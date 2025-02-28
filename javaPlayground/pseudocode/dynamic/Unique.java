6f61743547f0ce2a2b7fc85f836becd25dd12f56
the-last-crusaderr <guptasumit922@gmail.com>
2025-02-28 20:28:58 +0530

    dd

---
+++ javaPlayground/pseudocode/dynamic/Unique.java
@@ -0,0 +1,42 @@
+
+
+
+public class Unique{
+
+   
+  
+
+
+   public void sortUnique(int[] arr){
+      if(arr.length <= 1)
+	return arr;
+      
+      int i = 0, j = 1;
+      while(j < arr.length){
+         if(arr[j] == arr[j-1]){
+            while(arr[i )
+		 }
+         else{
+
+           while(arr[j] == arr[i]) j++;
+
+		 }		 
+		 
+
+
+
+	      }
+
+
+     sort(resultArr); 
+
+	   }
+
+   public static main(String [] args){
+
+
+
+
+	   }
+
+	}
