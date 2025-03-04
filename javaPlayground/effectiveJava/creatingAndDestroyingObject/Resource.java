4497a675c975f99a590bb208cb87bf09fe2090c7
the-last-crusaderr <guptasumit922@gmail.com>
2025-03-05 02:29:57 +0530

    added API like struc by hiding the implementaion from client programmer

--- javaPlayground/effectiveJava/creatingAndDestroyingObject/Resource.java
+++ javaPlayground/effectiveJava/creatingAndDestroyingObject/Resource.java
@@ -1,7 +1,9 @@
 class Resource implements AutoCloseable {
     private String name;
     private static Resource instance;    // Constructor
-    private Resource(String name) {
+    private static Mouse mouse;
+
+    protected Resource(String name) {
         this.name = name;
         System.out.println(name + " resource created.");
     }
@@ -15,6 +17,11 @@ class Resource implements AutoCloseable {
 	  }
 
 
+    public static Resource mouseFactory(String name){
+       mouse = new Mouse(name);
+       return mouse;
+	    }
+    
 
     public void use() {
         System.out.println("Using " + name + " resource.");
@@ -26,12 +33,38 @@ class Resource implements AutoCloseable {
         System.out.println(name + " resource destroyed.");
     }
 
+
+    // second sol: keeping the subclass nested and private
+
+    private static class Mouse extends Resource{
+
+       private Mouse(String name){
+          super(name);
+          System.out.println("The mouse resource has been created by "+ name);
+        }
+      }
+
+
+
+
     public static void main(String[] args) {
        // try (Resource res = new Resource("TestResource")) {
          //   res.use();
 	    Resource temp1 =  Resource.factoryConstructor("Build with static constructor");
             Resource temp2 =  Resource.factoryConstructor("Build with static constructor");
+            Resource temp3 =  Resource.mouseFactory("Coder1");
        // } // close() is automatically called here
     }
 }
 
+/* first sol - keep the class outside 
+   not proper hiding as programmer knows the existence of it 
+class Mouse extends Resource{
+
+       Mouse(String name){
+	  super(name);     
+          System.out.println("The mouse resource has been created by "+ name);
+        }   
+      }   
+
+      */
